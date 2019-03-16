2f07c2d5ee676e9b76dc96a6d57ccca5c6e601de
amirmk89 <amirmk89@gmail.com>
2019-03-16 16:23:35 +0200

    Added ntu class file support

--- Codes/kinetics_labels.py
+++ Codes/kinetics_labels.py
@@ -1,6 +1,9 @@
 
-def get_kinetics_names():
-  class_file = 'kinetics_classes.txt'
+def get_kinetics_names(ntu=False):
+  if ntu:
+      class_file = 'ntu_classes.txt'
+  else:
+      class_file = 'kinetics_classes.txt'
   class_names = []
 
   with open(class_file) as f:
